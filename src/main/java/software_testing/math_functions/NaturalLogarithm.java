dc6b9cd26c2e5d94f710a12e5f5548edb821fa48
Kirill Krakov <kirill170251@gmail.com>
2025-03-07 11:13:45 +0300

    add ln & log

---
+++ src/main/java/software_testing/math_functions/NaturalLogarithm.java
@@ -0,0 +1,31 @@
+package software_testing.math_functions;
+
+public class NaturalLogarithm {
+    public static double ln_with_epsilon(double x, double epsilon) {
+        if (x <= 0) throw new IllegalArgumentException("Значение X должно быть больше нуля");
+        if (x == 1) return 0;
+
+        int sign = 1;
+        if (x < 1) {
+            sign = -1;
+            x = 1 / x;
+        }
+
+        double y, result = 0, term;
+        int i = 1;
+        y = (x - 1) / x;
+        term = y;
+
+        while (Math.abs(term / (i * (1 - y))) >= epsilon) {
+            result += term / i;
+            term *= y;
+            i++;
+        }
+
+        return sign * result;
+    }
+
+    public static double ln(double x) {
+        return ln_with_epsilon(x, 0.00001);
+    }
+}
